da3c961063c6398ed90dfbb480ed87dea997d017
Kabir Bose <kabirbose04@gmail.com>
2023-11-08 22:44:14 -0500

    added ejs functionality

--- config/server.js
+++ config/server.js
@@ -1,10 +1,18 @@
+// modules
 const express = require("express");
-const app = express();
+const expressLayouts = require("express-ejs-layouts");
 
+const app = express();
 const port = 3000;
 
+// middleware
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
 
 app.listen(port, () => {
