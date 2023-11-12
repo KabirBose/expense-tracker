8f5346d53af26fdf22f62e85b797dd227b5c67e1
Kabir Bose <kabirbose04@gmail.com>
2023-11-12 12:21:32 -0500

    render documents onto html

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -7,16 +7,22 @@ const getExpenses = async () => {
     const response = await fetch("http://localhost:3000/api/expenses");
     const { data } = await response.json();
     data.map((expense) => {
-      expensesList.insertAdjacentHTML("beforebegin", `<p>${expense.name}</p>`);
+      const html = `
+    <div class="expense">
+        <div>
+            <h3 class="expense-name">${expense.name}</h3>
+            <h3 class="expense-description">${expense.description}</h3>
+        </div>
+        <h3 class="expense-price">$${expense.price}</h3>
+    </div>
+    `;
+      expensesList.insertAdjacentHTML("beforebegin", html);
     });
-    // expensesList.insertAdjacentHTML('afterbegin', )
   } catch (error) {
     console.error(`Failed to reach server, ${err}`);
   }
 };
 
 if (expensesHeading) {
-  expensesHeading.addEventListener("click", getExpenses);
+  window.addEventListener("load", getExpenses);
 }
-
-// render expenses
